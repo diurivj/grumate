5ae155aad3e6b2b36ee7aa1bc78feccc6f10dde1
Diego Vazquez <diurivj@icloud.com>
2018-05-10 13:35:26 -0500

    grumate app with security

--- routes/app.js
+++ routes/app.js
@@ -6,16 +6,23 @@ const uploads     = multer({dest: './public/uploads'});
 const nodemailer  = require('nodemailer');
 require('dotenv').config();
 
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
+
 router.get('/', (req, res, next) => {
   res.render('app');
 });
 
-router.get('/grumate', (req, res, next) => {
+router.get('/grumate', isLoggedIn, (req, res, next) => {
   res.render('grumateUser');
 });
 
-router.get('/social', (req, res, next) => {
+router.get('/social', isLoggedIn, (req, res, next) => {
   res.send('Aquí tendría que esta la parte social de grumate');
 });
+
+
 
 module.exports = router;
