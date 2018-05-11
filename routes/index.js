4eeb1f82002f48c4a9ee452ce37aeda8daf962da
joselink <consultor@confase4.com>
2018-05-10 19:39:57 -0500

    Despues de nav

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/subappi', (req, res, next) => {
+  res.redirect('http://subappi.herokuapp.com')
+});
 module.exports = router;
 
 
